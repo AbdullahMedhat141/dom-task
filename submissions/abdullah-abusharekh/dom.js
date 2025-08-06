59bd23599497f8a36e868d136b7e32a91714992c
AbdullahMedhat141 <134795680+AbdullahMedhat141@users.noreply.github.com>
2025-08-06 12:23:12 +0300

    Ready to submit

---
+++ submissions/abdullah-abusharekh/dom.js
@@ -0,0 +1,41 @@
+"use strict";
+
+//Task 1
+
+//1
+console.log(document.body.lastElementChild.tagName);
+//////////////////////////
+console.log(document.body.children.length); //3
+console.log(document.body.childNodes.length); //6
+/* the main difference between children and childNodes:
+ childNodes takes the white spaces like: '\n' as a text node beside the direct children nodes
+but children just takes the children nodes. */
+
+//2
+console.log(document.body.childNodes[0].nodeType); // 3
+console.log(document.body.childNodes[0].nodeName); // #text
+///////////////////////////////////
+const secondParagraph = document.querySelector("section p:nth-of-type(2)");
+console.log(secondParagraph.previousElementSibling); // siblings
+
+// Twist
+document.body.childNodes.forEach((node) => {
+  if (node.nodeType === 3) console.log(node);
+});
+/* Text nodes come from the new lines between elements,So they are
+basically white spaces and the browser turns them into nodes. */
+
+//Task 2
+
+const card = document.querySelector(".card");
+console.log(card.dataset.role);
+///////////////////////////////////
+const paragraph = document.querySelector(".card p");
+paragraph.textContent = "Welcome back, Admin";
+//////////////////////////////////
+card.classList.add("authenticated", "highlight");
+
+//Challenge
+console.log(card.classList.contains("card"));
+card.classList.remove("card");
+console.log([...card.classList]);
