43ea6c94434d2f655cca10d1219b6ad3f97fe689
AbdullahMedhat141 <134795680+AbdullahMedhat141@users.noreply.github.com>
2025-09-01 12:07:59 +0300

    Updated DOM task

--- submissions/abdullah-abusharekh/dom.js
+++ submissions/abdullah-abusharekh/dom.js
@@ -15,12 +15,21 @@ but children just takes the children nodes. */
 console.log(document.body.childNodes[0].nodeType); // 3
 console.log(document.body.childNodes[0].nodeName); // #text
 ///////////////////////////////////
-const secondParagraph = document.querySelector("section p:nth-of-type(2)");
+// const secondParagraph = document.querySelector("section p:nth-of-type(2)");
+const section = document.getElementsByTagName("section")[0];
+const secondParagraph = section.getElementsByTagName("p")[1];
+
 console.log(secondParagraph.previousElementSibling); // siblings
 
 // Twist
+// document.body.childNodes.forEach((node) => {
+//   if (node.nodeType === 3) console.log(node);
+// });
+
 document.body.childNodes.forEach((node) => {
-  if (node.nodeType === 3) console.log(node);
+  if (node.nodeType === 3 && node.textContent.trim() === "") {
+    console.log("Unexpected text node:", node);
+  }
 });
 /* Text nodes come from the new lines between elements,So they are
 basically white spaces and the browser turns them into nodes. */
@@ -28,7 +37,8 @@ basically white spaces and the browser turns them into nodes. */
 //Task 2
 const divEl = document.createElement("div");
 divEl.classList.add("card");
-divEl.setAttribute("data-role", "admin");
+// divEl.setAttribute("data-role", "admin");
+divEl.dataset.role = "admin";
 
 const h2 = document.createElement("h2");
 h2.textContent = "Access Panel";
